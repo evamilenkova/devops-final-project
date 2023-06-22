a16b3802b9dbc4b100a5643c04f4e0a93145bbfd
evamilenkova <emilenkova@yahoo.com>
2023-06-22 21:35:31 +0200

    like

--- final_project/settings.py
+++ final_project/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookshelf',
+        'USER': 'postgres',
+        'PASSWORD': 'mazoretka.1D',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
