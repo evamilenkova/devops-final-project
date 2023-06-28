36e23dc5e9cfb8c98139f2e09f6d7f83249ff8f9
evamilenkova <86674681+evamilenkova@users.noreply.github.com>
2023-06-28 21:35:03 +0200

    Update settings.py

--- final_project/settings.py
+++ final_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-)$%gjhbudr#*+6fl2lr-rv_*k7#y8^*k(be4luj3lh0k##hdia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['evamilenkova.pythonanywhere.com', 'localhost', 'mybookshelf.203035.com/']
+ALLOWED_HOSTS = ['evamilenkova.pythonanywhere.com', 'localhost', 'mybookshelf.203035.com']
 
 # Application definition
 
